2cc3b16a2827041e1c3dec15c10e96c61879378a
Guilherme Ramos <guilherme@raks.com.br>
2021-12-22 20:34:08 -0300

    Refactored EstoqueController to use MVC

--- src/features/estoque/controllers/EstoqueController.ts
+++ src/features/estoque/controllers/EstoqueController.ts
@@ -1,53 +1,49 @@
 import { Request, Response } from "express";
-import { v4 as uuid } from "uuid";
 import Database from "../../../core/data/connections/Database";
+import { Estoque } from "../../../core/data/database/entities/Estoque";
+import { Produto } from "../../../core/data/database/entities/Produto";
 
 export default class EstoqueController {
 	public async store(req: Request, res: Response) {
-		const connection = new Database().getConnection();
-
 		const { produto_uid, quantidade } = req.body;
 
-		const result = await connection.query(
-			`insert into estoque(uid, produto_uid, quantidade) values('${uuid()}', '${produto_uid}', ${Number(quantidade)})`
-		);
+		const produto = await Produto.findOne(produto_uid); //Busca o produto para verificar se existe e criar o item no estoque
 
-		console.log(result);
-
-		return res.status(200).send("Item criado no estoque");
+		if(produto){ //Valida a existência do produto
+			const estoque = await new Estoque(quantidade, produto).save(); //Adiciona item ao estoque
+			console.log(estoque);
+			return res.status(200).send("Item criado no estoque");
+		}else {
+			return res.status(400).send("Produto não encontrado");
+		}
 	}
 
 	public async index(req: Request, res: Response) {
-		const connection = new Database().getConnection();
-
-		const itens = await connection.query("select * from estoque");
+		const itens = await Estoque.find();
 
 		return res.json(itens);
 	}
 
 	public async view(req: Request, res: Response) {
-		const connection = new Database().getConnection();
-
 		const { uid } = req.params;
 
-		const estoque = await connection.query(
-			`select * from estoque where uid = '${uid}'`
-		);
-
-		return res.json(estoque);
+		const item = await Estoque.findOne(uid);
+		return res.json(item);
 	}
 
 	public async update(req: Request, res: Response) {
-		const connection = new Database().getConnection();
+		
 
 		const { uid } = req.params;
 
 		const { produto_uid, quantidade } = req.body;
 
-		if (produto_uid && quantidade) {
-			const result = await connection.query(`
-            UPDATE estoque SET produto_uid='${produto_uid}', quantidade=${Number(quantidade)} WHERE uid='${uid}'
-            `);
+		const produto = await Produto.findOne(produto_uid); //Busca o produto para verificar se existe e atualizar o item no estoque
+		const item = await Estoque.findOne(uid); //Busca item no estoque pelo uid para validar se existe e portanto pode ser atualizado
+
+		if (item && produto && quantidade) {
+			const estoque = await new Estoque(quantidade, produto, uid).save();
+
 			return res.status(200).send("Estoque atualizado");
 		} else {
 			return res.status(400).send("Parâmetros faltando");
@@ -55,16 +51,17 @@ export default class EstoqueController {
 	}
 
 	public async destroy(req: Request, res: Response) {
-		const connection = new Database().getConnection();
-
 		const { uid } = req.params;
 
-		const result = await connection.query(`
-            DELETE FROM estoque WHERE uid='${uid}'
-        `);
+		const item = await Estoque.findOne(uid);
 
-		console.log(result);
+		if(item){
+			const result = await Estoque.remove(item);
+			console.log(result);
+			return res.status(200).send("Item excluído com sucesso");
+		}else {
+			return res.status(404).send("Item não encontrado");
+		}
 
-		return res.status(200).send("Item excluído com sucesso");
 	}
 }
